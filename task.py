eb90863392f694ce0299194f03eaf0823554d460
tiran <r.tiranno@studenti.unisa.it>
2026-05-29 11:34:12 +0200

    implementazione sub-task 1 e 2

--- task.py
+++ task.py
@@ -11,7 +11,10 @@ def prodotto_scalare(v1: list, v2: list) -> float:
 
 def rango_matrice(m: list) -> int:
     """Sub-task 2: Calcola il rango di una matrice."""
-    pass
+
+    m_arr = np.array(m)
+    return int(np.linalg.matrix_rank(m_arr))
+
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
